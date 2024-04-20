e9a316cdac3495461a6eb9f7169d2cdae296b630
ZubSahal <zusah4508@oslomet.no>
2024-04-20 15:37:19 +0200

    biletter, film, KinoController

--- src/main/java/com/example/data1700_oblig3/Film.java
+++ src/main/java/com/example/data1700_oblig3/Film.java
@@ -1,4 +1,17 @@
 package com.example.data1700_oblig3;
 
 public class Film {
+    private String valg;
+
+    public Film(String valg){
+        this.valg=valg;
+    }
+
+    public String getValg(){
+        return valg;
+    }
+
+    public void setValg(String valg){
+        this.valg=valg;
+    }
 }
--- src/main/java/com/example/data1700_oblig3/KinoController.java
+++ src/main/java/com/example/data1700_oblig3/KinoController.java
@@ -1,4 +1,40 @@
 package com.example.data1700_oblig3;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class KinoController {
+
+    //Array hvor billettene skal lagres
+    private  final List<Billett> liste=new ArrayList<>();
+    @GetMapping("/hentFilmer")
+    public List<Film> hentefilm(){
+        List<Film> listMedFilmer=new ArrayList<>();
+        listMedFilmer.add(new Film("Barbie"));
+        listMedFilmer.add(new Film("Black panther"));
+        listMedFilmer.add(new Film("the rookie"));
+        return listMedFilmer;
+    }
+
+    @PostMapping("/lagre")
+    public void lagre(Billett biletter1){
+        liste.add(biletter1);
+    };
+
+
+
+    @GetMapping("/hentalle")
+    public List<Billett> hentAlle(){
+        return liste;
+    }
+
+    @GetMapping("/slettAlle")
+    public void selttAlle(){
+        liste.clear();
+    }
+
+
 }
